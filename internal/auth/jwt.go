694e7a7f61282ae6ed8a217b1e4c93b3710f881f
devitway <devitway@gmail.com>
2026-03-17 17:44:09 +0000

    v0.4: JWT auth (HS256), org_id in claims, backwards-compatible with plain IDs

---
+++ internal/auth/jwt.go
@@ -0,0 +1,63 @@
+// Copyright (c) 2026 Volkov Pavel | DevITWay
+// Licensed under the Business Source License 1.1. See LICENSE file for details.
+package auth
+
+import (
+	"errors"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var ErrInvalidToken = errors.New("invalid token")
+
+type Claims struct {
+	UserID   int64  `json:"uid"`
+	OrgID    string `json:"org"`
+	Username string `json:"sub"`
+	jwt.RegisteredClaims
+}
+
+type JWTService struct {
+	secret []byte
+	ttl    time.Duration
+}
+
+func NewJWTService(secret string, ttlHours int) *JWTService {
+	if secret == "" {
+		secret = "pusk-default-secret-change-me"
+	}
+	return &JWTService{
+		secret: []byte(secret),
+		ttl:    time.Duration(ttlHours) * time.Hour,
+	}
+}
+
+func (j *JWTService) Generate(userID int64, orgID, username string) (string, error) {
+	claims := Claims{
+		UserID:   userID,
+		OrgID:    orgID,
+		Username: username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(j.ttl)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Subject:   username,
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(j.secret)
+}
+
+func (j *JWTService) Validate(tokenStr string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(t *jwt.Token) (interface{}, error) {
+		return j.secret, nil
+	})
+	if err != nil {
+		return nil, ErrInvalidToken
+	}
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, ErrInvalidToken
+	}
+	return claims, nil
+}
